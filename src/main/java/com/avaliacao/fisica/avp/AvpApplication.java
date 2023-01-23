07f74b1ed35477f0d0e92c4109481b1087c484d0
Fredy Gomes <fredyultima2@gmail.com>
2023-01-23 11:12:04 -0300

    Create project

---
+++ src/main/java/com/avaliacao/fisica/avp/AvpApplication.java
@@ -0,0 +1,13 @@
+package com.avaliacao.fisica.avp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avaliacao/fisica/avp/AvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaliacao.fisica.avp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
