0545fce3232c45c6e56a43222b5aae8522f8322a
Fredy Gomes <fredyultima2@gmail.com>
2023-01-23 22:17:42 -0300

    Created the sql script of the CLiente table

---
+++ src/main/resources/db.migration/V1__CREATE_TABLE_CLIENTE.sql
@@ -0,0 +1,9 @@
+CREATE TABLE CLIENTE(
+    id int primary key,
+    nome varchar(100),
+    sobrenome varchar (100),
+    nascimentoDate date,
+    idade varchar(4),
+    telefone varchar (30),
+    cpf varchar(30)
+)
