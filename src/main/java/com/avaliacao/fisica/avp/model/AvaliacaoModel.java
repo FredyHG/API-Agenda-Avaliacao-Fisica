fdcb9d5cf8061a5b3b4e3c98933ad78280e25edd
Fredy Gomes <fredyultima2@gmail.com>
2023-01-23 21:48:55 -0300

    Created the attributes of the 'AvaliacaoModel' class

--- src/main/java/com/avaliacao/fisica/avp/model/AvaliacaoModel.java
+++ src/main/java/com/avaliacao/fisica/avp/model/AvaliacaoModel.java
@@ -1,4 +1,19 @@
 package com.avaliacao.fisica.avp.model;
 
+import java.util.Date;
+
 public class AvaliacaoModel {
+
+    private Long id;
+
+    private Date dataHora;
+
+    private boolean status;
+
+    private String alergias;
+
+    private String limitacoesFisicas;
+
+    private Long clientID;
+
 }
