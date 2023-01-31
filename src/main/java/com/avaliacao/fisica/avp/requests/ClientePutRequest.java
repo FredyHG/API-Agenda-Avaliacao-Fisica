07826cf7de9205db42a78d697c9c037729946d1c
Fredy Gomes <fredyultima2@gmail.com>
2023-01-31 12:50:14 -0300

    Created 'ClientePutRequest' class

---
+++ src/main/java/com/avaliacao/fisica/avp/requests/ClientePutRequest.java
@@ -0,0 +1,30 @@
+package com.avaliacao.fisica.avp.requests;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientePutRequest {
+
+    private String nome;
+
+    private String sobrenome;
+
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate dataNascimento;
+
+    private Long idade;
+
+    private String telefone;
+
+    private String cpf;
+
+}
